5c69cc189e5c98651d959b45583d6aa692abeca8
Steven Gomez <719513@student.dusd.net>
2023-12-06 13:52:08 -0800

    added columns, scrolling, 2/3 new scripts and yea
    the score text does not want to attach to the game controller, not sure what thats about but im out of time in class so ill ask on friday.
    
    start video 9 when working

--- FlappyBirdGomez/Assets/Scripts/Bird.cs
+++ FlappyBirdGomez/Assets/Scripts/Bird.cs
@@ -32,7 +32,9 @@ public class Bird : MonoBehaviour
 
     void OnCollisionEnter2D()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
         anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
---
+++ FlappyBirdGomez/Assets/Scripts/GameControl.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    public static GameControl instance;
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    public Text scoreText;
+    
+    private int score = 0;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy (gameObject);
+        }
+    }
+    public void BirdDied()
+    {
+        gameOverText.SetActive(true);
+        gameOver = true;
+    }
+
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString ();
+    }
+
+    private void Update()
+    {
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+}
